3c7c62bc288e064d62939397f40eae5dbd985230
denzelbautista <denzel.bautista@utec.edu.pe>
2023-05-22 16:27:23 -0500

    añadiendo_archivosc++

---
+++ pruebascmasmas.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout << "Ingrese un numero: ";
+    cin >> x;
+    cout << "El numero ingresado es: " << x << endl;
+    return 0;
+}
